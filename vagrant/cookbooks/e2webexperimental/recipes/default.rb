c60b62f17f36d85c930bed0b176e7963dfdad553
Jay Bonci <jay@bonci.net>
2013-04-22 11:21:44 -0400

    Add in other Mason component

--- vagrant/cookbooks/e2webexperimental/recipes/default.rb
+++ vagrant/cookbooks/e2webexperimental/recipes/default.rb
@@ -8,5 +8,6 @@
 #
 
 package 'libmason-perl' #Mason2
+package 'libexception-class-perl' #Bug in ubuntu's package, actually requires this
 package 'libmoose-perl' #modern Perl object system framework
 package 'libnamespace-autoclean-perl' #Perl module to remove all imported symbols at the end of the compile cycle
